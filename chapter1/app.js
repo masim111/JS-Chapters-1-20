3acb5709f3659da83ee79b5fe1c2c9977a7548f2
muhammadaasim Asim <mohammadaasim111@gmail.com>
2020-06-05 15:56:55 +0500

    first commit

---
+++ chapter1/app.js
@@ -0,0 +1,35 @@
+
+
+var password=+prompt("Enter your password")
+if (password ==16){
+    alert("Valid password")
+}
+else if (password < 14){
+    alert(" Valid password")
+
+}
+else
+{
+   alert ("Error! Please enter a Valid password")
+}
+
+
+
+
+  
+
+   alert('Welcome to JS Land...\n Happy Coding!')
+
+   alert ("Welcome to JS land...")
+
+
+
+
+
+
+
+
+
+   
+
+
---
+++ chapter2/app.js
@@ -0,0 +1,51 @@
+
+var num1 = prompt("First Name: ");
+var num2 = prompt("Last_Name: ");
+alert(num1 + num2);
+
+
+
+var age = prompt("Enter your age");
+alert(age +  "  " + "Year Old")
+
+var myText = "Certiied Mobile Application Development";
+    alert (myText);
+
+    var p =[];
+p[0] = "PIZZA \r\n"; 
+p[1] = "PIZZ \r\n" ;
+p[2] = "PIZ \r\n";
+p[3] = "PI \r\n";
+p[4] = "P \r\n";
+
+alert(p );
+
+// var name = prompt("enter your name:");
+//     alert ("name, " + name);
+
+    var name = prompt("Enter your Email:");
+    alert (name);
+
+var myText = " ------------ ஜ۩۞۩ஜ ------------ " ;
+    alert (myText);
+    
+   
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+   
+
+
